9abaeb4a733985449303ff5f3f74c66a3607d43a
Nguyen Manh Hung <hungnm.pnv@gmail.com>
2021-04-26 22:53:41 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div id="container">
+            <canvas ref="c" id="canvas" width="800" height="800"></canvas>
+        </div>
+        <right-side-bar-component @upload-img="uploadImg" />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {fabric} from 'fabric'
+import RightSideBarComponent from '@/components/RightSideBarComponent'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: { RightSideBarComponent },
+    data() {
+        return {
+            canvas: null,
+            angle: 0
+        }
+    },
+
+    mounted() {
+        window.addEventListener('resize', this.initCanvas)
+
+        this.$nextTick(() => {
+            const ref = this.$refs.c
+            this.canvas = new fabric.Canvas(ref, { backgroundColor: '#cdcdcd' })
+            this.initCanvas()
+
+            this.canvas.on({
+                'object:moving': (e) => {
+                    e.target.opacity = 0.5
+                },
+                'object:modified': (e) => {
+                    e.target.opacity = 1
+                }
+            })
+        })
+    },
+
+    methods: {
+        initCanvas() {
+            if (!this.canvas) return
+
+            this.canvas.setDimensions({
+                width: window.innerWidth - 301,
+                height: window.innerHeight
+            })
+        },
+
+        uploadImg(e) {
+            const fileType = e.target.files[0].type
+            const listFileTypes = ['image/jpeg', 'image/png', 'image/svg+xml']
+            if (!listFileTypes.includes(fileType)) return
+            const url = URL.createObjectURL(e.target.files[0])
+
+            if (fileType === 'image/svg+xml') {
+                this.uploadImgWithSVG(url)
+            } else {
+                this.uploadImgNormal(url)
+            }
+        },
+
+        uploadImgWithSVG(url) {
+            fabric.loadSVGFromURL(url, (objects, options) => {
+                let svg = fabric.util.groupSVGElements(objects, options)
+                this.canvas.add(svg)
+            })
+        },
+
+        uploadImgNormal(url) {
+            fabric.Image.fromURL(url, (myImg) => {
+                this.canvas.add(myImg)
+            })
+        }
+    }
 }
 </script>
 
 <style>
+body {
+    margin: 0;
+    padding: 0%;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    display: flex;
+    flex-direction: row;
 }
 </style>
---
+++ src/components/RightSideBarComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="right-sidebar">
+        <input id="imgUpload" type="file" accept="image/*" @change.prevent="uploadImg" />
+        <hr />
+        <div class="library">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RightSideBarComponent',
+    data() {
+        return {
+            images: [
+
+            ]
+        }
+    },
+    methods: {
+        uploadImg(e) {
+            this.$emit("upload-img", e)
+        }
+    }
+}
+</script>
+
+<style>
+.right-sidebar {
+    width: 300px;
+    height: 100vh;
+    border-left: 1px solid #000;
+    padding: 8px;
+    box-sizing: border-box;
+    /* background-color: red; */
+}
+</style>
